e8e6a766a84fa56c79fff7ac6c233cfa1945e078
lucassant <lucasfdesantana095@gmail.com>
2026-05-07 09:09:45 -0300

    fix: bug camelCase

---
+++ porcentagem-react/src/components/headerTop.jsx
@@ -0,0 +1,13 @@
+import '../style/index.css'
+
+function headerTop(){
+    return(
+        <>
+            <header>
+                <h2>calculadora porcentagem</h2>
+            </header>
+        </>
+    )
+};
+
+export default headerTop
---
+++ porcentagem-react/src/components/mainPage.jsx
@@ -0,0 +1,14 @@
+function mainPage(){
+    return(
+        <>
+            <main>
+                <div>
+                    <input type='number' placeholder="Valor" />
+                    <input type="number" placeholder="Porcentagem" />
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default mainPage
